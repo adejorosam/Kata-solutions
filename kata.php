4752ed7138e4665213244dd27cf7cff5e6818ca6
Samson Adejoro <samsonadejoro@gmail.com>
2019-12-03 11:10:26 +0100

    Update kata.php

--- kata.php
+++ kata.php
@@ -1,9 +1,3 @@
-/*You're going on a trip with some students and 
-it's up to you to keep track of how much money each student has. 
-Each student has some fives, tens, and twenties. 
-Your job is to return the name of the student with the most money. 
-If every student has the same amount, then return "all". */
-
 function expense_track($stud_money){
   $highest = 0;
   $stud_money = [2000=>"Tolu", 500=>"Samson",1000=>"Olawale"];
